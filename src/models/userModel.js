40262562889b46240ac24b6d9f70de7e82f653d8
KULDEEP SINGH SIKARWAR <117976159+kuldeep021998@users.noreply.github.com>
2024-08-15 13:43:43 +0530

    minor

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,11 +2,11 @@ const db = require("../config/dbConfig");
 
 const User = {
   create: (userData, callback) => {
-    const query = "INSERT INTO users (name, email) VALUES (?, ?)";
+    const query = "INSERT INTO admin (name, email) VALUES (?, ?)";
     db.query(query, [userData.name, userData.email], callback);
   },
   findAll: (callback) => {
-    const query = "SELECT * FROM users";
+    const query = "SELECT * FROM admin";
     db.query(query, callback);
   },
 };
